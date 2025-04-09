7b120bf9ae19cf2034729d8052ee826cba75e058
Erwan ROBIN <erobin.pro@outlook.fr>
2025-04-09 15:25:26 +0200

    fix: correct filename format in PDF generation by removing brackets around language

--- src/components/Download/useDownloadPDF.jsx
+++ src/components/Download/useDownloadPDF.jsx
@@ -16,7 +16,7 @@ const useDownloadPDF = () => {
     const day = today.getDate().toString().padStart(2, "0");
     const month = (today.getMonth() + 1).toString().padStart(2, "0");
     const year = today.getFullYear();
-    const fileName = `CV_ErwanRobin_${cvType.toUpperCase()}_[${language}]_(${day}-${month}-${year}).pdf`;
+    const fileName = `CV_ErwanRobin_${cvType.toUpperCase()}_${language}_(${day}-${month}-${year}).pdf`;
 
     const options = {
       filename: fileName,
